6e36b133c7f13569982f8fd9578ab8b0d5636dcc
dipujasim247 <dipujasim247@gmail.com>
2019-11-27 16:24:14 +0600

    laravel serve and npm run watch are merge together and App.vue set up

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,18 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
+
+mix.webpackConfig({
+    plugins: [
+        new BrowserSyncPlugin({
+            files: [
+                'app/**/*',
+                'public/**/*',
+                'resources/views/**/*',
+                'routes/**/*'
+            ]
+        })
+    ]
+}
+
+);
